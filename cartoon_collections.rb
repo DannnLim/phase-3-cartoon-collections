dc3e12204bb4561018a3e7194017afd101426d4f
DannnLim <Dannylim-@hotmail.com>
2022-10-23 20:51:38 +1100

    commit tree 155dfef72f3bbc2d53a03239628110ff11ea67e2

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,29 @@
-def roll_call_dwarves# code an argument here
+def roll_call_dwarves(dwarf)# code an argument here
   # Your code here
+  dwarf.map.with_index do |name, i|
+    puts "#{i + 1}. #{name}"
+  end
 end
 
-def summon_captain_planet# code an argument here
+def summon_captain_planet(array)# code an argument here
   # Your code here
+  array.map do |w|   
+   w.capitalize() + "!"
+  end
 end
 
-def long_planeteer_calls# code an argument here
+def long_planeteer_calls(characters)# code an argument here
   # Your code here
+  nu = characters.any? do |c|
+    c.length > 4
+  end
+  return nu
 end
 
-def find_the_cheese# code an argument here
+def find_the_cheese(cheese)# code an argument here
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
+  return cheese.detect do |cheeses|
+  cheese_types.include?(cheeses)
+  end
 end
